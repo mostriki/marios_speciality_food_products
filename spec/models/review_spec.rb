f0d3cdbf8b9d7d8bc9ec5c1ab5a8c2291ea4451c
Riki Montgomery <mostriki820@gmail.com>
2017-11-17 14:03:36 -0800

    pass 9 tests and add additional untested validations

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,9 +1,12 @@
 require 'rails_helper'
 
 describe Review do
+  it { should belong_to :product }
   it { should validate_presence_of :author }
   it { should validate_presence_of :content }
   it { should validate_presence_of :rating }
   it { should validate_presence_of :product_id }
-  it { should belong_to :product}
+  # it {should validate_length_of(:content). is_at_least(50).is_at_most(150)}
+  # it {validates_numericality_of :rating, less_than_or_equal_to: 5}
+  # it {validates_numericality_of :product_id, less_than_or_equal_to: 50)}
 end
