37b1d2da3498ae3e7b50ac15f9c3d70564d47669
Riki Montgomery <mostriki820@gmail.com>
2017-11-17 14:50:30 -0800

    add index, show, new, create, and update routes with functionality

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,37 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Product.all.limit(10)
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+    #  flash[:notice] = "Thanks for adding to our menu!"
+      redirect_to  products_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    @product= Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to products_path
+    else
+      render :edit
+    end
+  end
+
+private
+  def product_params
+    params.require(:product).permit(:name, :cost, :origin)
+  end
+end
