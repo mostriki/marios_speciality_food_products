28b25448ac901cb7666cacd0ecd8486d71b7725a
Riki Montgomery <mostriki820@gmail.com>
2017-11-17 15:06:57 -0800

    complete CRUD functionality for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -21,6 +21,10 @@ class ProductsController < ApplicationController
     end
   end
 
+  def edit
+     @product = Product.find(params[:id])
+   end
+
   def update
     @product= Product.find(params[:id])
     if @product.update(product_params)
@@ -28,6 +32,12 @@ class ProductsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path
   end
 
 private
