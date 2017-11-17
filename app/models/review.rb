c9ae5aa26dc5433ae8280a4687160fd93e7a3e2e
Riki Montgomery <mostriki820@gmail.com>
2017-11-17 15:57:34 -0800

    pass 12 validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,8 +5,8 @@ class Review < ActiveRecord::Base
   validates :content, :presence => true
   validates :content, length: { minimum: 50, maximum: 250 }
   validates :rating, :presence => true
-  # validates :rating, numericality: { only_integer: true }
-  # validates :rating, inclusion: { in: 1..5, "must be less than or equal to 5" }
+  validates :rating, numericality: { only_integer: true }
+  validates :rating, inclusion: { in: 1..5, message: "must be less than or equal to 5" }
   validates :product_id, :presence => true
   # validates :product_id, numericality: { only_integer: true }
   # validates :password, inclusion: { in: 1..50, "must be less than or equal to 50" }
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -6,7 +6,7 @@ describe Review do
   it { should validate_presence_of :content }
   it { should validate_presence_of :rating }
   it { should validate_presence_of :product_id }
-  it {should validate_length_of(:content). is_at_least(50).is_at_most(250)}
-  # it {should validate_numericality_of(:rating).is_less_than_or_equal_to(5)}
-  # it {should validate_numericality_of(:product_id).is_less_than_or_equal_to(50)}
+  it { should validate_length_of(:content). is_at_least(50).is_at_most(250) }
+  it { should validate_numericality_of(:rating).is_less_than_or_equal_to(5) }
+  # it { should validate_numericality_of(:product_id).is_less_than_or_equal_to(50) }
 end
