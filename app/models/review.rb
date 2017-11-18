1c4f0f658c1f59642bcf622e25cce2f0726c37eb
Riki Montgomery <mostriki820@gmail.com>
2017-11-17 16:01:51 -0800

    pass 13 validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -8,7 +8,7 @@ class Review < ActiveRecord::Base
   validates :rating, numericality: { only_integer: true }
   validates :rating, inclusion: { in: 1..5, message: "must be less than or equal to 5" }
   validates :product_id, :presence => true
-  # validates :product_id, numericality: { only_integer: true }
-  # validates :password, inclusion: { in: 1..50, "must be less than or equal to 50" }
+  validates :product_id, numericality: { only_integer: true }
+  validates :product_id, inclusion: { in: 1..50, message: "must be less than or equal to 50" }
 
 end
