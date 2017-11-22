39ff44cd102b2a749907b42e8d1d2772e0c0539e
Riki Montgomery <mostriki820@gmail.com>
2017-11-22 17:34:07 -0500

    made seeds run

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,28 @@
-# Product.destroy_all
+Product.destroy_all
 
-# 50.times do |index|
+50.times do |index|
+  Product.create!(name: Faker::Food.dish, cost: Faker::Number.between(1, 20), origin: Faker::Address.country)
+end
+
+Review.destroy_all
+
+250.times do |index|
+  product = products[Faker::Number.between(1, product_count)]
+  product.reviews.create!( author: Faker::GameOfThrones.character, content: Faker::Lorem.characters(51), rating: Faker::Number.between(1, 5))
+end
+
+# Product.destroy_all
+#
+# product_count = 50
+#
+# products = (1..product_count).map do |index|
 #   Product.create!(name: Faker::Food.dish, cost: Faker::Number.between(1, 20), origin: Faker::Address.country)
 # end
 #
 # Review.destroy_all
 #
 # 250.times do |index|
-#   Review.create!( author: Faker::GameOfThrones.character, content: Faker::Lorem.characters(51), rating: Faker::Number.between(1, 5), product_id: Faker::Number.between(1, 50))
+#   product_index = Faker::Number.between(0, product_count-1)
+#   product = products[product_index]
+#   product.reviews.create!( author: Faker::GameOfThrones.character, content: Faker::Lorem.characters(51), rating: Faker::Number.between(1, 5))
 # end
