0ac90aa92f353d95c2165dfaa435437327c93ddc
Riki Montgomery <mostriki820@gmail.com>
2017-11-22 09:27:09 -0500

    pass first integration test for adding a new product

---
+++ spec/features/add_product_pages_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe "the add a product process" do
+  it "adds a new product" do
+    visit products_path
+    click_link 'Create'
+    fill_in 'Name', :with => 'Turkey'
+    fill_in 'Cost', :with => '20'
+    fill_in 'Origin', :with => 'Alabama'
+    click_on 'Create Product'
+    expect(page).to have_content 'Products'
+  end
+
+  # it "gives error when no name is entered" do
+  #   visit new_product_path
+  #   click_on 'Create Product'
+  #   expect(page).to have_content 'errors'
+  # end
+end
