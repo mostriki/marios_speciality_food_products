a44406e0afde1e229657d5f83d94f5d2dc3a7a4e
Riki Montgomery <mostriki820@gmail.com>
2017-11-22 09:31:27 -0500

    pass test to give an error when no description is entered

--- spec/features/edit_review_pages_spec.rb
+++ spec/features/edit_review_pages_spec.rb
@@ -15,15 +15,15 @@ describe "the edit a review process" do
     expect(page).to have_content '2'
   end
 #
-#   it "gives error when no description is entered" do
-#     product = Product.create(:name => 'Home stuff', :cost => '20', :origin => 'Alabama')
-#     review = Review.create(:author => 'Allyria Desmond', :content => 'jsenssas8wqruhui6gledrrtraecefsadd9qkv0q8icwvdbpo5p', :rating => 3, :product_id => product.id)
-#     visit product_path(product)
-#     click_on 'Edit'
-#     fill_in 'Author', :with => ''
-#     fill_in 'Content', :with => ''
-#     fill_in 'Rating', :with => ''
-#     click_button 'Update Review'
-#     expect(page).to have_content 'errors'
-#   end
+  it "gives error when no description is entered" do
+    product = Product.create(:name => 'Home stuff', :cost => '20', :origin => 'Alabama')
+    review = Review.create(:author => 'Allyria Desmond', :content => 'jsenssas8wqruhui6gledrrtraecefsadd9qkv0q8icwvdbpo5p', :rating => 3, :product_id => product.id)
+    visit product_path(product)
+    click_on 'Edit'
+    fill_in 'Author', :with => ''
+    fill_in 'Content', :with => ''
+    fill_in 'Rating', :with => ''
+    click_button 'Update Review'
+    expect(page).to have_content 'errors'
+  end
 end
