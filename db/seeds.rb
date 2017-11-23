fa0f425c864a437220d7a16e8467c6e8ec36a0a2
Riki Montgomery <mostriki820@gmail.com>
2017-11-22 23:43:29 -0500

    pass 21 tests and successfully seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,21 @@
 Product.destroy_all
 
+products = []
+
 50.times do |index|
-  Product.create!(name: Faker::Food.dish, cost: Faker::Number.between(1, 20), origin: Faker::Address.country)
+  products << Product.create!(name: Faker::Food.dish, cost: Faker::Number.between(1, 20), origin: Faker::Address.country)
 end
+
 
 Review.destroy_all
 
 250.times do |index|
-  product = products[Faker::Number.between(1, product_count)]
-  product.reviews.create!( author: Faker::GameOfThrones.character, content: Faker::Lorem.characters(51), rating: Faker::Number.between(1, 5))
+  Review.create!( author: Faker::GameOfThrones.character, content: Faker::Lorem.characters(51), rating: Faker::Number.between(1, 5), product_id: Faker::Number.between(1, 50))
 end
 
+
+
+# ///Experiment demonstrated to me////
 # Product.destroy_all
 #
 # product_count = 50
@@ -22,7 +27,6 @@ end
 # Review.destroy_all
 #
 # 250.times do |index|
-#   product_index = Faker::Number.between(0, product_count-1)
-#   product = products[product_index]
+#   product = products[Faker::Number.between(0, product_count -1)]
 #   product.reviews.create!( author: Faker::GameOfThrones.character, content: Faker::Lorem.characters(51), rating: Faker::Number.between(1, 5))
 # end
